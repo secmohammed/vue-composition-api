e7e1b431cdc311632af51b082867fc1e283e096e
mohammed osama <mohammedosama@ieee.org>
2020-04-23 05:01:27 +0200

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <p>{{ number }}</p>
+    <p>{{string}}</p>
+        
+    </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api'
+
+export default {
+    setup() {
+        // to create a reactive property, based on its type.
+        const number = ref(0)
+        const string = ref('Hello') // ref takes any primitive datatype.
+        setInterval(() => {
+            number.value++
+        }, 1000)
+        return {
+            number,
+            string
+        }
+    }
+};
+</script>
